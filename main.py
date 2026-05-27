1b804fd01f4ce05322ed2e1e282daf16b16e0e7e
DILSHODBEK <dilshodsattar26@gmail.com>
2026-05-27 06:23:46 +0500

    Initial commit: setup main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import sys
+import json
+
+class validatorSetchWorker:
+    def __init__(self):
+        self.buffer = []
+    def append_metric(self, val):
+        self.buffer.append(val)
+        return len(self.buffer)
+
+if __name__ == '__main__':
+    obj = validatorSetchWorker()
+    print("Worker engine initialized.")
